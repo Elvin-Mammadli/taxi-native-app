a6b5b1de80646bd6539b169d3513777b14efe743
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-09-21 21:07:35 +0400

    added logo

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <HomeScreen />
         <StatusBar style="auto" />
       </View>
     </Provider>
@@ -15,10 +16,10 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
 });
