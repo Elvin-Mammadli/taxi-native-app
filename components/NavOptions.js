e14cf652a0f596510d3cbbbe3399c3ed6adf1f0d
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-09-22 11:34:09 +0400

    created Screens

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,45 +1,52 @@
-import React from 'react';
-import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
-import tw from 'twrnc';
+import { Icon } from "@rneui/base";
+import React from "react";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import tw from "twrnc";
+// import { Icon } from "@rneui/themed";
 
 const data = [
   {
-    id: '1',
-    title: 'Get a ride',
-    image: 'https://links.papareact.com/3pn',
-    screen: 'MapScreen'
+    id: "1",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
   },
   {
-    id: '2',
-    title: 'Order food',
-    image: 'https://links.papareact.com/28w',
-    screen: 'EatScreen'
-  }
-]
+    id: "2",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatScreen",
+  },
+];
 
 const NavOptions = () => {
   return (
     <FlatList
       data={data}
       horizontal
-      keyExtractor={item => item.id}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity
-          style={tw`p-2 pl-6 pt-4 pb-8 bg-gray-200 m-2 w-40`}
-        >
+        <TouchableOpacity style={tw`p-2 pl-6 pt-4 pb-8 bg-gray-200 m-2 w-40`}>
           <View>
             <Image
               source={{
-                uri: item.image
+                uri: item.image,
               }}
-              style={{ width: 120, height: 120, resizeMode: 'contain' }}
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
             />
-            <Text>{item.title}</Text>
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              type="antdesign"
+              name="arrowright"
+              color="white"
+            />
           </View>
         </TouchableOpacity>
       )}
+      style={{ flexGrow: 0 }}
     />
-  )
-}
+  );
+};
 
 export default NavOptions;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
-import tw from 'twrnc';
-import NavOptions from '../components/NavOptions';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import tw from "twrnc";
+import NavOptions from "../components/NavOptions";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={tw`bg-white h-full`}>
+    <SafeAreaView style={tw`bg-white flex-1`}>
       <View style={tw`p-5`}>
         <Image
           source={{
-            uri: 'https://links.papareact.com/gzs'
+            uri: "https://links.papareact.com/gzs",
           }}
           style={{
             width: 100,
             height: 100,
-            resizeMode: 'contain',
+            resizeMode: "contain",
           }}
         />
       </View>
 
       <NavOptions />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
