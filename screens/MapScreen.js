7877a64358aee87bf2f876c1488274038755ee22
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-09-22 15:01:58 +0400

    map added

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,17 @@
-import { Text, View } from "react-native";
 import React from "react";
+import { Text, View } from "react-native";
+import tw from "twrnc";
+import Map from "../components/Map";
+import MapView from "react-native-maps";
 
 const MapScreen = () => {
   return (
     <View>
-      <Text>MapScreen</Text>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+
+      <View style={tw`h-1/2`}></View>
     </View>
   );
 };
